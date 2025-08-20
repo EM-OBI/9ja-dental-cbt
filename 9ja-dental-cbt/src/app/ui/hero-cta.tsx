54fa5ee8841b3fc58752071aac70a7108ae1f0d4
EM-OBI <gbassey1@byupathway.edu>
2025-08-20 06:42:46 +0100

    working on something

---
+++ 9ja-dental-cbt/src/app/ui/hero-cta.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function HeroCta() {
+    const imageSrc = "";
+    return (
+        <>
+            <div>
+                <h1>Welcome to 9ja Dental CBT</h1>
+                <p>Nigeria's number one all-inclusive exam preparation hub for preclinical and clinical dental student</p>
+                <Link
+                    href={"/quiz"}
+                >
+                    Quiz mode
+                </Link>
+                <Link
+                    href={"/study"}
+                >
+                    Study mode
+                </Link>
+            </div>
+            <Image 
+                src={imageSrc}
+                width={}
+                height={}
+                className=""
+                alt=""
+            />
+        </>
+    )
+}
